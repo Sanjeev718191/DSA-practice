6ef92cf31db2beaa4b7c8a6285970d89b409fd2c
Sanjeev <sanjeev718191@gmail.com>
2025-07-29 23:21:02 +0530

    Graph Day 2 - 130

---
+++ Graph/leecode130.java
@@ -0,0 +1,44 @@
+public class leecode130 {
+    // link - https://leetcode.com/problems/surrounded-regions/description/
+}
+
+public class Solution {
+    public void solve(char[][] board) {
+        int rows = board.length;
+        int cols = board[0].length;
+        for (int i = 0; i < rows; i++) {
+            if (board[i][0] == 'O') dfs(board, i, 0);
+            if (board[i][cols - 1] == 'O') dfs(board, i, cols - 1);
+        }
+
+        for (int j = 0; j < cols; j++) {
+            if (board[0][j] == 'O') dfs(board, 0, j);
+            if (board[rows - 1][j] == 'O') dfs(board, rows - 1, j);
+        }
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (board[i][j] == 'O') {
+                    board[i][j] = 'X';
+                }
+            }
+        }
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (board[i][j] == 'P') {
+                    board[i][j] = 'O';
+                }
+            }
+        }
+    }
+
+    private void dfs(char[][] board, int i, int j) {
+        if (i < 0 || j < 0 || i >= board.length || j >= board[0].length || board[i][j] != 'O') {
+            return;
+        }
+        board[i][j] = 'P';
+        dfs(board, i + 1, j);
+        dfs(board, i - 1, j);
+        dfs(board, i, j + 1);
+        dfs(board, i, j - 1);
+    }
+}
