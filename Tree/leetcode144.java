796d39b09b0e0b3b4b15e40246ce0e7105d7eff0
Sanjeev <sanjeev718191@gmail.com>
2025-08-23 11:12:52 +0530

    Tree Day 2 - 144

--- Tree/leetcode144.java
+++ Tree/leetcode144.java
@@ -17,3 +17,23 @@ class Solution {
         preOrder(root.right, ls);
     }
 }
+
+// Iterative
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        
+        List<Integer> ls = new ArrayList<>();
+        if(root == null)
+            return ls;
+        
+        Stack<TreeNode> st = new Stack<>();
+        st.push(root);
+        while(!st.isEmpty()){
+            root = st.pop();
+            ls.add(root.val);
+            if(root.right != null) st.push(root.right);
+            if(root.left != null ) st.push(root.left);
+        }
+        return ls;
+    }
+}
