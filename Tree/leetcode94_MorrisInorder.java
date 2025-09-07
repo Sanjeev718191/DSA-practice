746d3c86d592013af45703309993cedadf07399a
Sanjeev <sanjeev718191@gmail.com>
2025-09-07 21:47:47 +0530

    Tree Day 9 - 94

---
+++ Tree/leetcode94_MorrisInorder.java
@@ -0,0 +1,31 @@
+public class leetcode94_MorrisInorder {
+    // link - https://leetcode.com/problems/binary-tree-inorder-traversal/description/
+}
+
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> ls = new ArrayList<>();
+        if(root == null) return ls;
+        TreeNode curr = root;
+        while(curr != null) {
+            if(curr.left == null) {
+                ls.add(curr.val);
+                curr = curr.right;
+            } else {
+                TreeNode prev = curr.left;
+                while(prev.right != null && prev.right != curr) {
+                    prev = prev.right;
+                }
+                if(prev.right == null) {
+                    prev.right = curr;
+                    curr = curr.left;
+                } else {
+                    prev.right = null;
+                    ls.add(curr.val);
+                    curr = curr.right;
+                }
+            }
+        }
+        return ls;
+    }
+}
