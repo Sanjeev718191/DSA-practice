7f24e2d9177d33c2323740098e71c5c36ae1d5a7
Sanjeev <sanjeev718191@gmail.com>
2025-09-07 21:47:35 +0530

    Tree Day 9 - MinBST

---
+++ BST/MinBST.java
@@ -0,0 +1,16 @@
+public class MinBST {
+    // link - https://www.geeksforgeeks.org/problems/minimum-element-in-bst/1
+}
+
+class Tree {
+    // Function to find the minimum element in the given BST.
+    int minValue(Node node) {
+        if(node == null)
+            return -1;
+        Node curr = node;
+        while(curr.left != null){
+            curr = curr.left;
+        }
+        return curr.data;
+    }
+}
